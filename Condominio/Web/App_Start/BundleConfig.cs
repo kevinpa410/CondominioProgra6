cdfbc65f09fe756cd1cfd04554080166a4213ec5
kevinpa410 <kevinpaniagua410@gmail.com>
2023-02-10 21:35:05 -0600

    DB Updated

--- Condominio/Web/App_Start/BundleConfig.cs
+++ Condominio/Web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Web
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.Cosmo.min.css",
                       "~/Content/site.css"));
         }
     }
--- DB/CondominioDB.sql
+++ DB/CondominioDB.sql
@@ -1,1 +1,174 @@
-Create database Condominio 
+Create database Condominio
+Go
+use Condominio
+Go
+
+Create Table Usuario(
+ID int IDENTITY (1,1) not null,
+correo varchar(50),
+contrasenna varchar(20),
+nombre varchar(20),
+apellido varchar(20),
+IDRol int,
+activo int
+);
+Go
+Alter Table Usuario add constraint PK_Usuario primary key (ID);
+Go
+ALTER TABLE Usuario ADD CONSTRAINT FKUsuario_Rol FOREIGN KEY(IDRol)REFERENCES Rol(ID)
+Go
+
+--------------------------------------------------------------------------------------
+Create Table Rol(
+ID int IDENTITY (1,1) not null,
+descripcion varchar(20)
+);
+Go
+alter table Rol add constraint PK_Rol primary key (ID);
+Go
+
+--------------------------------------------------------------------------------------
+Create Table Residencias(
+ID int IDENTITY (1,1) not null,
+IDUsuario int not null,
+IDEstado int,
+IDPlanCobro int,
+IDIncidencias int,
+CantPersonas int,
+AnnoInicio date,
+CantCarros int
+
+);
+Go
+alter table Residencias add constraint PK_Residencias primary key (ID);
+Go
+ALTER TABLE Residencias ADD CONSTRAINT FKResidencias_Usuario FOREIGN KEY(IDUsuario)REFERENCES Usuario(ID)
+Go
+ALTER TABLE Residencias ADD CONSTRAINT FKResidencias_EstadoResidencias FOREIGN KEY(IDEstado)REFERENCES EstadoResidencias(ID)
+Go
+ALTER TABLE Residencias ADD CONSTRAINT FKResidencias_Incidencias FOREIGN KEY(IDIncidencias)REFERENCES Incidencias(ID)
+Go
+ALTER TABLE Residencias ADD CONSTRAINT FKResidencias_PlanesCobro FOREIGN KEY(IDPlanCobro)REFERENCES PlanesCobro(ID)
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table EstadoResidencias(
+ID int IDENTITY (1,1) not null,
+Descripcion varchar(50),
+);
+Go
+alter table EstadoResidencias add constraint PK_EstadoResidencias primary key (ID);
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table Incidencias(
+ID int IDENTITY (1,1) not null,
+IDEstado int,
+Descripcion varchar(50),
+);
+Go
+alter table Incidencias add constraint PK_Incidencias primary key (ID);
+Go
+ALTER TABLE Incidencias ADD CONSTRAINT FKIncidencias_Estado FOREIGN KEY(IDEstado)REFERENCES EstadoIncidencia(ID)
+Go
+
+
+--------------------------------------------------------------------------------------
+
+Create Table EstadoIncidencia(
+ID int IDENTITY (1,1) not null,
+Descripcion varchar(50),
+);
+Go
+alter table EstadoIncidencia add constraint PK_EstadoIncidencia primary key (ID);
+Go
+--------------------------------------------------------------------------------------
+
+Create Table Reservaciones(
+ID int IDENTITY (1,1) not null,
+IDUsuario int,
+IDAreaComunal int,
+Descripcion varchar(100),
+Fecha date,
+Hora time(7),
+CantTiempo time(7)
+);
+Go
+alter table Reservaciones add constraint PK_Reservaciones primary key (ID);
+Go
+ALTER TABLE Reservaciones ADD CONSTRAINT FKReservaciones_Usuario FOREIGN KEY(IDUsuario)REFERENCES Usuario(ID)
+Go
+ALTER TABLE Reservaciones ADD CONSTRAINT FKReservaciones_AreaComunal FOREIGN KEY(IDAreaComunal)REFERENCES AreaComunal(ID)
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table AreaComunal(
+ID int IDENTITY (1,1) not null,
+Descripcion varchar (100)
+);
+Go
+alter table AreaComunal add constraint PK_AreaComunal primary key (ID);
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table RubroCobro(
+ID int IDENTITY (1,1) not null,
+Monto numeric,
+Descripcion varchar (100),
+
+);
+Go
+alter table RubroCobro add constraint PK_RubroCobro primary key (ID);
+Go
+--------------------------------------------------------------------------------------
+
+Create Table PlanesCobro(
+ID int IDENTITY (1,1) not null,
+IDEstado int,
+Total numeric,
+Mes date
+);
+Go
+alter table PlanesCobro add constraint PK_PlanesCobro primary key (ID);
+Go
+ALTER TABLE PlanesCobro ADD CONSTRAINT FKPlanesCobro_EstadoPlanesCobro FOREIGN KEY(IDEstado)REFERENCES EstadoPlanesCobro(ID)
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table EstadoPlanesCobro(
+ID int IDENTITY (1,1) not null,
+Descripcion varchar(50),
+);
+Go
+alter table EstadoPlanesCobro add constraint PK_EstadoPlanesCobro primary key (ID);
+Go
+--------------------------------------------------------------------------------------
+
+Create Table RubroPlanes(
+IDRubro int not null,
+IDPlanes int not null,
+);
+Go
+alter table RubroPlanes add constraint PK_RubroPlanes primary key (IDRubro,IDPlanes);
+Go
+ALTER TABLE RubroPlanes ADD CONSTRAINT FKRubroPlanes_Rubro FOREIGN KEY(IDRubro)REFERENCES RubroCobro(ID)
+Go
+ALTER TABLE RubroPlanes ADD CONSTRAINT FKRubroPlanes_Planes FOREIGN KEY(IDPlanes)REFERENCES PlanesCobro(ID)
+Go
+
+--------------------------------------------------------------------------------------
+
+Create Table Informacion(
+ID int IDENTITY (1,1) not null,
+Descripcion varchar(100),
+Imagen varbinary (max),
+
+);
+
+alter table Informacion add constraint PK_Informacion primary key (ID);
+Go
